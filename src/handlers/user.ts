a288ba2fbb1bf4344ea8dbbcecd0ba2dc0f03ff0
DanGeorge1995 <dangeorgeburghelea@yahoo.com>
2024-01-29 12:26:42 +0200

    first commit

---
+++ src/handlers/user.ts
@@ -0,0 +1,45 @@
+import prisma from "../db";
+import { comparePasswords, createJWT, hashPassword } from "../modules/auth";
+
+export const registerUser = async (req, res, next) => {
+  console.log({ body: req.body });
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username: req.body.username,
+        password: await hashPassword(req.body.password),
+        email: req.body.email,
+      },
+    });
+    // const token = createJWT(user);
+    // res.json({ token });
+    res.json({ user });
+  } catch (err) {
+    console.log(err);
+    err.type = "input";
+    next(err);
+  }
+};
+
+export const loginUser = async (req, res, next) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username: req.body.username,
+      },
+    });
+
+    const isValid = await comparePasswords(req.body.password, user.password);
+
+    if (!isValid) {
+      res.status(401);
+      res.json({ message: "Nope" });
+      return;
+    }
+
+    const token = createJWT(user);
+    res.json({ token });
+  } catch (err) {
+    next(err);
+  }
+};
---
+++ src/router.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+// import { registerUser } from "./handlers/user";
+
+const router = Router();
+
+export default router;
